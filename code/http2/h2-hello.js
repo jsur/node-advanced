17f17a7b6a2c9bbce7ac8dfca22da6c32e1d308c
Julius Suominen <julius.suominen@gmail.com>
2018-04-06 19:52:44 +0200

    joo joo juu

--- code/http2/h2-hello.js
+++ code/http2/h2-hello.js
@@ -2,11 +2,13 @@ const http2 = require('http2')
 const fs = require('fs')
 
 const server = http2.createSecureServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.crt')
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.crt')
 }, (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain' })
+  res.writeHead(200, {'Content-Type': 'text/html' })
   res.end('<h1>Hello World</h1>') // JUST LIKE HTTP!
 })
 server.on('error', (err) => console.error(err))
-server.listen(3000)
+server.listen(3000, () => {
+  console.log('listiening');
+})
